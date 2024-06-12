629c70de82f573f2583a82a32b3c41c627a2b589
nouraabdelfattah <143145113+nouraabdelfattah@users.noreply.github.com>
2024-06-13 01:45:44 +0300

    Update Experience.jsx

--- src/components/5-Experience/Experience.jsx
+++ src/components/5-Experience/Experience.jsx
@@ -4,6 +4,8 @@ import { FaReact } from "react-icons/fa";
 import { RiJavascriptFill } from "react-icons/ri";
 import { FaNode } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { IoLogoVercel } from "react-icons/io5";
+import { FaBootstrap } from "react-icons/fa";
 
 import "./experience.css"
 export default function Experience() {
@@ -14,12 +16,13 @@ export default function Experience() {
       <div className="exp-iqons row">
       <div className="html col-md-3 col-4"><FaHtml5/><span className="iqon-span">Html5</span></div>
       <div className=" css col-md-3 col-4"><IoLogoCss3/><span className="iqon-span">Css3</span></div>
+       <div className="node col-md-3 col-4"><FaBootstrap/><span className="iqon-span">Bootstrap</span></div>
+       <div className="js col-md-3 col-4"><RiJavascriptFill/><span className="iqon-span">JavaScript</span></div>
       <div className=" react col-md-3 col-4"><FaReact/><span className="iqon-span">React JS</span></div>
-      <div className="js col-md-3 col-4"><RiJavascriptFill/><span className="iqon-span">JavaScript</span></div>
       <div className="node col-md-3 col-4"><FaNode/><span className="iqon-span">Node JS</span></div>
       <div className="node col-md-3 col-4"><FaGithub/><span className="iqon-span">Github</span></div>
-      <div className="node col-md-3 col-4"><FaNode/><span className="iqon-span">Html5</span></div>
-      <div className="node col-md-3 col-4"><FaNode/><span className="iqon-span">Html5</span></div>
+      <div className="node col-md-3 col-4"><IoLogoVercel/><span className="iqon-span">Vercel</span></div>
+      
       </div>
     </div>
   )
