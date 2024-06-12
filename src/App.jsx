9a4ee5efc79d81b708082c444653832f8aa874e1
nouraabdelfattah <143145113+nouraabdelfattah@users.noreply.github.com>
2024-06-13 02:26:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { MdKeyboardArrowUp } from "react-icons/md";
 import About from './components/3-about-me/About'
 import Experience from './components/5-Experience/Experience'
 import { useEffect, useState } from 'react';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './components/Home';
 
 
@@ -24,23 +23,23 @@ window.addEventListener("scroll", () => {
 },)
 
   return (
-    <BrowserRouter>
+   
     <div id='up' className=''>
       <Header/>
       <Routes>
-      <Route path="/" element={<Home />} />
-          <Route path="/hero" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/contact" element={<Contact />} />
+      {<Home />
+          <Hero />
+          <About />
+        <Main />
+          <Experience />
+          <Contact />
       </Routes>
        <a style={{opacity:scroll?1:0, transition:"1s"}} href="#up">
        <button className='scroll-top'><MdKeyboardArrowUp/></button>
        </a>
      
     </div>
-    </BrowserRouter>
+   
   )
 }
 
