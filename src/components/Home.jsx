4b87947257701c4ea412dc014ab0ab5c748e875e
nouraabdelfattah <143145113+nouraabdelfattah@users.noreply.github.com>
2024-07-10 04:26:43 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Header from './1-header/Header'
+
 import Hero from './2-hero/Hero'
 import About from './3-about-me/About'
 import Main from './4-main/Main'
@@ -7,13 +6,14 @@ import Experience from './5-Experience/Experience'
 import Contact from './6-contact/Contact'
 
 export default function Home() {
+  
   return (
     <div>
-      <div id="home"><Hero/></div>
-      <div id="about"><About/></div>
-      <div id="projects"><Main/></div>
-      <div id="experience"><Experience/></div>
-      <div id="contact"><Contact/></div>
-    </div>
+    <div id="home"><Hero/></div>
+    <div id="about"><About/></div>
+    <div id="projects"><Main/></div>
+    <div id="experience"><Experience/></div>
+    <div id="contact"><Contact/></div>
+  </div>
   )
 }
