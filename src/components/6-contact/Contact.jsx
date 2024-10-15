ce34320a757f3d1efc71ea85f68470f008ffdee9
nouraabdelfattah <143145113+nouraabdelfattah@users.noreply.github.com>
2024-10-15 05:29:07 +0300

    Update Contact.jsx

--- src/components/6-contact/Contact.jsx
+++ src/components/6-contact/Contact.jsx
@@ -13,14 +13,14 @@ export default function Contact() {
 
  
   return (
-    <div className="container-fluid" id="#contact">
-       <h2 className="main-title">CONTACT</h2>
+    <div className="container-fluid contact-parent" id="#contact">
+       <h2 className="main-title">Contact</h2>
     <div className="container-fluid contact">
      
 <div className="contactMe row">
      <div className="contact-left-section col-md-5 col-sm-12">
       <h3>Drop Me a Message</h3>
-      <p>I hope to hear your opinion and I will be happy to read your message</p>
+      <p>I hope to know your opinion and I will be happy to read your message</p>
      <span className="gmail"><CgMail className="gmail-iqon"/></span><span className="gmail-p">nouraabdelfattah1@gmail.com</span><br/>
      <span className="gmail"><MdLocationOn className="gmail-iqon"/></span><span className="gmail-p">Cairo, Egypt</span>
      </div>
@@ -48,11 +48,11 @@ export default function Contact() {
      <hr className="line"></hr>
      <div className='container-fluid footer'>
      <div className="footer-iqons">
-     <span className='footer-iqon' > <a href="https://github.com/nouraabdelfattah"><FaGithub/></a></span>
-      <span className='footer-iqon'><a href="https://x.com/noura_Aelfattah?t=8WMwOaFhQMIwNh9rCK2QuQ&s=09"><FaXTwitter/></a></span>
-      <span className='footer-iqon'><a href="https://www.instagram.com/nouraabd.elfattah?utm_source=qr&igsh=MXVpOXo4N2JoN2I1cg=="><FaInstagram/></a></span>
-      <span className='footer-iqon'><a href="https://www.linkedin.com/in/noura-abd-elfattah-0515b1253?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FaLinkedin/></a></span>
-      <span className='footer-iqon'><a href="https://www.facebook.com/profile.php?id=100006201288319&sfnsn=scwspwa&mibextid=RUbZ1f"><FaFacebook/></a></span>
+     <span className='footer-iqon' > <a className="text-white" href="https://github.com/nouraabdelfattah"><FaGithub/></a></span>
+      <span className='footer-iqon'><a className="text-white" href="https://x.com/noura_Aelfattah?t=8WMwOaFhQMIwNh9rCK2QuQ&s=09"><FaXTwitter/></a></span>
+      <span className='footer-iqon'><a className="text-white" href="https://www.instagram.com/nouraabd.elfattah?utm_source=qr&igsh=MXVpOXo4N2JoN2I1cg=="><FaInstagram/></a></span>
+      <span className='footer-iqon'><a className="text-white" href="https://www.linkedin.com/in/noura-abd-elfattah-0515b1253?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FaLinkedin/></a></span>
+      <span className='footer-iqon '><a className="text-white" href="https://www.facebook.com/profile.php?id=100006201288319&sfnsn=scwspwa&mibextid=RUbZ1f"><FaFacebook/></a></span>
      </div>
      <p className="copyright">&copy;2024 Noura Abd Elfattah</p>
     </div>
