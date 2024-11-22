c3bc4cf83f49b19031a8275e91d3ba4a1e340411
Noura Abd Elfattah <143145113+nouraabdelfattah@users.noreply.github.com>
2024-11-22 04:30:49 +0200

    Update Experience.jsx

--- src/components/5-Experience/Experience.jsx
+++ src/components/5-Experience/Experience.jsx
@@ -6,7 +6,7 @@ import { FaNode } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { IoLogoVercel } from "react-icons/io5";
 import { FaBootstrap } from "react-icons/fa";
-
+import {motion} from "framer-motion"
 import "./experience.css"
 export default function Experience() {
  
@@ -15,14 +15,14 @@ export default function Experience() {
       <h2 className="main-title">Experience</h2>
       <span className="sub-span"></span>
       <div className="exp-iqons row">
-      <div className="html col-md-3 col-4"><FaHtml5/><span className="iqon-span">Html5</span></div>
-      <div className=" css col-md-3 col-4"><IoLogoCss3/><span className="iqon-span">Css3</span></div>
-      <div className="node col-md-3 col-4"><FaBootstrap/><span className="iqon-span">Bootstrap</span></div>
-      <div className="js col-md-3 col-4"><RiJavascriptFill/><span className="iqon-span">JavaScript</span></div>
-      <div className=" react col-md-3 col-4"><FaReact/><span className="iqon-span">React JS</span></div>
-      <div className="node col-md-3 col-4"><FaNode/><span className="iqon-span">Node JS</span></div>
-      <div className="node col-md-3 col-4"><FaGithub/><span className="iqon-span">Github</span></div>
-      <div className="node col-md-3 col-4"><IoLogoVercel/><span className="iqon-span">Vercel</span></div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className="html col-md-3 col-4"><FaHtml5/><span className="iqon-span">Html5</span></motion.div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className=" css col-md-3 col-4"><IoLogoCss3/><span className="iqon-span">Css3</span></motion.div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className="node col-md-3 col-4"><FaBootstrap/><span className="iqon-span">Bootstrap</span></motion.div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className="js col-md-3 col-4"><RiJavascriptFill/><span className="iqon-span">JavaScript</span></motion.div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className=" react col-md-3 col-4"><FaReact/><span className="iqon-span">React JS</span></motion.div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className="node col-md-3 col-4"><FaNode/><span className="iqon-span">Node JS</span></motion.div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className="node col-md-3 col-4"><FaGithub/><span className="iqon-span">Github</span></motion.div>
+      <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1,opacity:1}} className="node col-md-3 col-4"><IoLogoVercel/><span className="iqon-span">Vercel</span></motion.div>
       
       </div>
     <hr />
